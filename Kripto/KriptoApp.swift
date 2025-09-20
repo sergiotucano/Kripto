ad3df74381a1914d421e7704857554106c009550
Sergio Clemente <sergiotucano@icloud.com>
2025-09-19 22:40:59 -0300

    Initial Commit

---
+++ Kripto/KriptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KriptoApp.swift
+//  Kripto
+//
+//  Created by Sergio Clemente on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct KriptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
